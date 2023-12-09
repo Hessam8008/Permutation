b8dc7d07aad13f669d6c6a10d1b760822d14ec95
hessam hosseini <hosseini@iraanbaba.local>
2023-12-09 12:08:23 +0330

    Add permutation methods and buffer for save result.

---
+++ BufferWriter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutation
+{
+    public sealed class BufferWriter
+    {
+        private static BufferWriter _instance;
+        public const string FileName = "result.txt";
+        public static BufferWriter GetInstance(int total)
+        {
+            _instance ??= new BufferWriter(total);
+            return _instance;
+        }
+
+        private readonly List<string> _buffer;
+        private int _counter = 0;
+        private int _total = 0;
+        private int _rows = 0;
+
+        private BufferWriter(int total)
+        {
+            File.Delete(FileName);
+            _buffer = new List<string>();
+            _total = total;
+        }
+
+        public void Add(string str)
+        {
+            _buffer.Add(str);
+            _counter++;
+            if (_counter >= 1_000_000)
+            {
+                Save();
+                Clear();
+                _rows += _counter;
+                _counter = 0;
+            }
+        }
+
+        public void Save()
+        {
+            var output = string.Join(Environment.NewLine, _buffer);
+            File.AppendAllText(FileName, output);
+            Console.WriteLine($"Write buffer {_rows} of {_total} [{1.0*_rows / _total * 100:00.0}%] ...");
+        }
+
+        public void Clear()
+        {
+            _buffer.Clear();
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using Permutation;
+
+var str = "A3.&G,c}\\QOo-@hJ";
+int n = str.Length;
+int total = Factorial(n);
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"░░░▒▒▓ Calculate all permutation for '{str}' ");
+Console.WriteLine($"░░░▒▒▓ Created by Hessam Hosseini 09-12-2023 ");
+Console.WriteLine($"░░░▒▒▓ Total charachters : {n}");
+Console.WriteLine($"░░░▒▒▓ Total permutations: {total}");
+Console.WriteLine($"░░░▒▒▓ Output file       : {BufferWriter.FileName}");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+var buff = BufferWriter.GetInstance(total);
+
+permute(str, 0, n - 1, buff);
+
+buff.Save();
+
+Console.ForegroundColor = ConsoleColor.Green ;
+Console.WriteLine("... Write completed ...");
+Console.WriteLine("....... Finished ......");
+Console.ForegroundColor = ConsoleColor.Gray;
+
+static void permute(string str, int l, int r, BufferWriter buff)
+{
+    if (l == r)
+        buff.Add(str);
+    else
+    {
+        for (int i = l; i <= r; i++)
+        {
+            str = swap(str, l, i);
+            permute(str, l + 1, r, buff);
+            str = swap(str, l, i);
+        }
+
+    }
+}
+
+/** 
+* Swap Characters at position 
+* @param a string value 
+* @param i position 1 
+* @param j position 2 
+* @return swapped string 
+*/
+static string swap(string a, int i, int j)
+{
+    char temp;
+    char[] charArray = a.ToCharArray();
+    temp = charArray[i];
+    charArray[i] = charArray[j];
+    charArray[j] = temp;
+    string s = new string(charArray);
+    return s;
+}
+
+
+int Factorial(int f)
+{
+    if (f == 0)
+        return 1;
+    else
+        return f * Factorial(f - 1);
+}
